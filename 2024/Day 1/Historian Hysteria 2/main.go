68a9fd2f42c5dc7baaae8b4e7024d68e710c4d84
Anmol Chawla <anmolchawla10@gmail.com>
2024-12-01 14:20:04 +0530

    Day 1 : Problem 2 - minor improvement

--- 2024/Day 1/Historian Hysteria 2/main.go
+++ 2024/Day 1/Historian Hysteria 2/main.go
@@ -24,6 +24,9 @@ func main() {
 
 	var first_line []int
 	var second_line []int
+
+	occurences_map := make(map[int]int)
+
 	for file_reader.Scan() {
 		numbers := strings.Split(file_reader.Text(), "   ")
 		first_number, error := strconv.Atoi(strings.Replace(numbers[0], " ", "", -1))
@@ -34,22 +37,16 @@ func main() {
 
 		first_line = append(first_line, first_number)
 		second_line = append(second_line, second_number)
+		occurences_map[second_number] += 1
 	}
 
 	slices.Sort(first_line)
 	slices.Sort(second_line)
 
-	occurences_map := make(map[int]int)
-
-	for i := range second_line {
-		occurences_map[second_line[i]] += 1
-	}
-
 	similarity_score := 0
 
 	for i := range first_line {
 		second_line_repeats := occurences_map[first_line[i]]
-
 		similarity_score += first_line[i] * second_line_repeats
 	}
 
