d3d340bfe7c6a174a8563df3a14a2d3753191ea8
Anmol Chawla <anmolchawla10@gmail.com>
2024-12-03 10:31:17 +0530

    Day 2 : Both problems

---
+++ 2024/Day 2/Red-Nosed Reports 2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e.Error())
+		panic(e)
+	}
+}
+
+func safe(report []int) bool {
+	var increasing = report[0] < report[len(report)-1]
+	for i := 1; i < len(report); i++ {
+		if increasing && (report[i] <= report[i-1] || report[i]-report[i-1] > 3) ||
+			!increasing && (report[i] >= report[i-1] || report[i-1]-report[i] > 3) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	file, error := os.Open("input.txt")
+	check(error)
+
+	file_reader := bufio.NewScanner(file)
+
+	safe_reports := 0
+
+	for file_reader.Scan() {
+		line := file_reader.Text()
+		numbers := strings.Split(line, " ")
+		var number_list []int
+		for i := range numbers {
+			number, error := strconv.Atoi(numbers[i])
+			check(error)
+			number_list = append(number_list, number)
+		}
+
+		if safe(number_list) {
+			safe_reports += 1
+		} else {
+			for i := range number_list {
+				var number_range = make([]int, len(number_list))
+				copy(number_range, number_list)
+				if safe(append(number_range[0:i], number_range[i+1:]...)) {
+					safe_reports += 1
+					break
+				}
+			}
+		}
+	}
+
+	log.Println(safe_reports)
+}
