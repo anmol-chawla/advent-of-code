5e8c6a15c52a91892ef39a4b5a1a95f6a7cff75c
Anmol Chawla <anmolchawla10@gmail.com>
2024-12-04 10:17:08 +0530

    Day 3 : both problems

---
+++ 2024/Day 3/Mull It Over 2/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e.Error())
+		panic(e)
+	}
+}
+
+func main() {
+	input_file, error := os.Open("input.txt")
+	check(error)
+
+	file_reader := bufio.NewScanner(input_file)
+
+	var input_lines []string
+	for file_reader.Scan() {
+		input_lines = append(input_lines, file_reader.Text())
+	}
+
+	var matched_substrings []string
+	for i := range input_lines {
+		match_condition := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
+		matched_substrings = append(matched_substrings, match_condition.FindAllString(input_lines[i], -1)...)
+	}
+
+	total := 0
+	enabled := true
+
+	for i := range matched_substrings {
+		if matched_substrings[i] == "do()" {
+			enabled = true
+		} else if matched_substrings[i] == "don't()" {
+			enabled = false
+		} else if enabled {
+			substring := matched_substrings[i]
+			match_condition := regexp.MustCompile(`\d+`)
+			numbers := match_condition.FindAllString(substring, -1)
+
+			first_number, error := strconv.Atoi(numbers[0])
+			check(error)
+
+			second_number, error := strconv.Atoi(numbers[1])
+			check(error)
+
+			total += first_number * second_number
+		}
+	}
+
+	log.Print(total)
+}
